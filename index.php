2ad666b88062bb0e227fd474249d2cf244f701e3
YNTKTS15907 <fikrirayhanft15@gmail.com>
2023-11-10 21:54:08 +0700

    mengedit file index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur id cupiditate esse tempora quaerat totam dolore nemo nam provident non excepturi molestiae voluptas eius, repellat asperiores iusto tenetur. Facilis, commodi.</p>
 </body>
 </html>
